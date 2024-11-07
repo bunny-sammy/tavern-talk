39ce7fe74acbf35fcfe462c8e137bb6f01b857d4
Alysson <alyssonwa02@gmail.com>
2024-11-07 13:17:42 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          AAAAAAAAAAAAAAAAAAAAAAAAAAAA
         </p>
       </div>
       <p className="read-the-docs">
