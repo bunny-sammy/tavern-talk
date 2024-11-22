e76f806f0ba9c437959a84cb5231ebe4661d6bea
Alysson <alyssonwa02@gmail.com>
2024-11-22 15:16:28 -0300

    tela de login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,21 @@
+import "./style.scss"
 const Login = () => { 
     return(
-        <div>
-            Home Page
+        <div className="login_page">
+            <img src="/assets/logo.png" alt="Logo Tavern Talk" className='logo_login' />
+            <div className="input_login">
+                <h1>Email</h1>
+                <input type="email" className="gradient-border"/>
+                <h1>Senha</h1>
+                <input type="senha" className="gradient-border"/>
+
+            {/* <button>esqueci minha senha</button> */}
+            {/* <Link href="/page"> */}
+                <button>
+                    <img src="/assets/login-button.svg" alt="" />
+                </button>
+            {/* </Link> */}
+            </div>
         </div>
     );
 }
