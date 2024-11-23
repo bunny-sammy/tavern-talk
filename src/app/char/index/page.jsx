d85bc82247934f118e30bbc4e8af467efdd64193
Alysson <alyssonwa02@gmail.com>
2024-11-22 23:53:56 -0300

    bordas no login e bordas no char/index bonito

--- src/app/char/index/page.jsx
+++ src/app/char/index/page.jsx
@@ -2,15 +2,30 @@ import NavBar from "@/components/NavBar"
 import "./style.scss"
 import Link from "next/link";
 
+
 export default function Index(){
     return(
         <div className="char_index">
             <NavBar/>
             <h1>TavernTalk</h1>
-            <div className="button_char gradient-box" >
-                <button>Eldrick Stormwind</button>
+            <div className="button_char">
+                <Link href="/char/show" className="char_link gradient-box">
+                    <div className="texto_botao">
+                    <span className="personagem">Eldrick Stormwind</span>
+                    <span className="estatisticas"> For: 10, Des: 14, Con: 12, Int: 18, Sab: 16, Car: 12 </span>
+                    </div>
+                    <img src="/assets/seta.svg" alt="seta" className='seta'/>
+                </Link>
+                <Link href="/char/show" className="char_link gradient-box">
+                    <div className="texto_botao">
+                    <span className="personagem">Kelthar Shadowbane</span>
+                    <span className="estatisticas"> For: 10, Des: 14, Con: 12, Int: 18, Sab: 16, Car: 12 </span>
+                    </div>
+                    <img src="/assets/seta.svg" alt="seta" className='seta'/>
+                </Link>
             </div>
         </div>
+        
     )
 }
 
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -8,10 +8,14 @@ const Login = () => {
             <img src="/assets/logo.png" alt="Logo Tavern Talk" className='logo_login' />
             <div className="input_login">
                 <h1>Email</h1>
-                <input type="email" className="gradient-border"/>
+                <div  className=" teste gradient-box">
+                <input type="email"/>
+                </div>
                 <h1>Senha</h1>
-                <input type="senha" className="gradient-border"/>
-
+                <div className="teste gradient-box">
+                <input type="password"/>
+                </div>
+                
             {/* <button>esqueci minha senha</button> */}
             <Link href="/char/index">
                 <button>
