4c14073517be4b06ba2cde581af73ffd266c1f10
bunnysammy_ <davidenacrene@gmail.com>
2024-11-26 09:13:01 -0300

    :wrench: Resolvendo possível vulnerabilidade

--- src/pages/api/users/list.js
+++ src/pages/api/users/list.js
@@ -4,8 +4,8 @@ import User from "/models/user";
 export default async function GET(req, res) {
   try {
     await mongodb.connect();
-    const users = await User.find();
-    res.status(200).json(users);
+    const users = await User.countDocuments();
+    res.status(200).json({users_count: users});
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
